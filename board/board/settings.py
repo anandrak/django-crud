8f91298a64f7b1eda4e848a6b43b6d415d1705bb
leeingyu <krpeppermint100@gmail.com>
2019-10-01 20:50:42 +0900

    correct some typo

--- board/board/settings.py
+++ board/board/settings.py
@@ -165,4 +165,4 @@ AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-django_heroku.settings.(locals())
+django_heroku.settings(locals())
